82b3733754312c09910c6104784e76dcf8786eb8
Sohail Ahmed <sohail.comsians@gmail.com>
2022-07-02 01:34:17 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ebay Scraper Python Package from RapidAPI
+
+This package is developed to scrape eBay products using RapidAPI. 
+You can use [eBay Scraper](https://rapidapi.com/domainarcher/api/ebay-scraper/)
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ebay-scraper',
+    version='1.0',    
+    description='eBay products scraper python package.',
+    url='https://github.com/meetsohail/ebay-scraper',
+    author='Sohail Ahmed',
+    author_email='meetsohail360@gmail.com',
+    license='',
+    packages=['ebay-scraper'],
+)
+
